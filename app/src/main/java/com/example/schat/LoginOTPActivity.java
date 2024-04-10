175141fcaa61d89517ebfd71bce2ba3d33103cd7
20520827 <20520827@gm.uit.edu.vn>
2024-04-10 23:23:51 +0700

    Add OTP verification and re-send OTP.

--- app/src/main/java/com/example/schat/LoginOTPActivity.java
+++ app/src/main/java/com/example/schat/LoginOTPActivity.java
@@ -1,5 +1,6 @@
 package com.example.schat;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +17,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.schat.utils.AndroidUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.Firebase;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
@@ -26,6 +30,8 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.TimeUnit;
 
 public class LoginOTPActivity extends AppCompatActivity {
@@ -57,9 +63,21 @@ public class LoginOTPActivity extends AppCompatActivity {
 
         phoneNumber = getIntent().getExtras().getString("phone");
         sendOTP(phoneNumber, false);
+
+        confirm.setOnClickListener(v -> {
+            String enteredOTP = inputOTP.getText().toString();
+            PhoneAuthCredential paCred = PhoneAuthProvider.getCredential(verificationCode, enteredOTP);
+            logIn(paCred);
+            setInProgress(true);
+        });
+
+        resendOTP.setOnClickListener(v -> {
+            sendOTP(phoneNumber, true);
+        });
     }
 
     void sendOTP(String phoneNumber, boolean isResent){
+        startTimeOutTimer();
         setInProgress(true);
         PhoneAuthOptions.Builder builder = PhoneAuthOptions.newBuilder(mAuth);
         builder.setPhoneNumber(phoneNumber);
@@ -94,8 +112,45 @@ public class LoginOTPActivity extends AppCompatActivity {
         }
     }
 
+    void startTimeOutTimer() {
+        resendOTP.setEnabled(false);
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                timeOut--;
+                resendOTP.setText("Gửi lại mã OTP sau " + timeOut +" giây.");
+                if(timeOut <= 0)
+                {
+                    timeOut = 60L;
+                    timer.cancel();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            resendOTP.setEnabled(true);
+                        }
+                    });
+                }
+            }
+        }, 0, 1000);
+    }
+
     void logIn(PhoneAuthCredential phoneAuthCredential) {
         //Login then go to next activity
+        setInProgress(true);
+        mAuth.signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                setInProgress(false);
+                if(task.isSuccessful()){
+                    Intent intent = new Intent(LoginOTPActivity.this, UsernameLoginActivity.class);
+                    intent.putExtra("phone", phoneNumber);
+                    startActivity(intent);
+                }else {
+                    AndroidUtil.showToast(getApplicationContext(), "Mã OTP không đúng");
+                }
+            }
+        });
     }
 
     void setInProgress(boolean isInProgress){
