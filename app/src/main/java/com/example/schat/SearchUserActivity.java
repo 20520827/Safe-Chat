408a376caf5f4ac8495ac9bd1fb7ee0b84793931
20520827 <20520827@gm.uit.edu.vn>
2024-04-15 21:37:58 +0700

    Add search activity UI.

--- app/src/main/java/com/example/schat/SearchUserActivity.java
+++ app/src/main/java/com/example/schat/SearchUserActivity.java
@@ -1,14 +1,21 @@
 package com.example.schat;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SearchUserActivity extends AppCompatActivity {
+    EditText searchInput;
+    ImageButton backBtn;
+    ImageButton searchBtn;
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +27,28 @@ public class SearchUserActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        searchInput = findViewById(R.id.search_input);
+        backBtn = findViewById(R.id.backButton);
+        searchBtn = findViewById(R.id.search_user_btn);
+        rv = findViewById(R.id.search_user_rv);
+
+        searchInput.requestFocus();
+        backBtn.setOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        searchBtn.setOnClickListener(v->{
+            String searchTerm = searchInput.getText().toString();
+            if(searchTerm.isEmpty() || searchTerm.length() < 3) {
+                searchInput.setError("Tên người dùng không hợp lệ");
+                return;
+            }
+            setSearchRecycleView(searchTerm);
+        });
+
+        
+    }
+
+    void setSearchRecycleView(String searchTerm) {
     }
 }
